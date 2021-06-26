8ded496c90b7918f43d3d4f9cb9b186fe06d2290
Fernando Perez Diez <perezdiezf@gmail.com>
2021-06-26 14:03:32 -0300

    with async await finally

--- api/src/routes/post.js
+++ api/src/routes/post.js
@@ -4,7 +4,37 @@ const { Router } = require('express');
 
 const router = Router();
 
-router.post ('/recipes', (req, res, next) => {
+// router.post ('/recipes', (req, res, next) => {
+//     const id = uuidv4();
+//     const recipe = {...req.body, id};
+//     const {diet} = req.body
+//     if(!req.body.name || !req.body.resume) {
+//         return res.status(404).send({ 
+                 
+//             message: "'nombre' y 'resumen del plato' son obligatorios",
+//         });
+//     }   
+//         const createdRecipe = Recipe.create(recipe)
+//         .then(result => {
+//              for (var i=0; i < req.body.diet.length; i++){                
+//             result.addDiets(diet[i], {through:'recipesDiet'})            
+//             }
+//         })
+//         .then(() =>{
+//             const findCreated = Recipe.findOne({
+//                          where: {
+//                          name: req.body.name
+//                         },
+//                         include: Diets
+//             });
+//             findCreated.then(result => res.send(result));
+//         })
+            
+  
+//   })
+
+
+  router.post ('/recipes', async (req, res, next) => {
     const id = uuidv4();
     const recipe = {...req.body, id};
     const {diet} = req.body
@@ -14,25 +44,27 @@ router.post ('/recipes', (req, res, next) => {
             message: "'nombre' y 'resumen del plato' son obligatorios",
         });
     }   
-        const createdRecipe = Recipe.create(recipe)
-        .then(result => {
+        try {const createdRecipe = await Recipe.create(recipe)
+        
              for (var i=0; i < req.body.diet.length; i++){                
-            result.addDiets(diet[i], {through:'recipesDiet'})            
+                createdRecipe.addDiets(diet[i], {through:'recipesDiet'})            
             }
-        })
-        .then(() =>{
-            const findCreated = Recipe.findOne({
+       
+        
+            const findCreated = await Recipe.findOne({
                          where: {
                          name: req.body.name
                         },
                         include: Diets
             });
-            findCreated.then(result => res.send(result));
-        })
+            res.send(findCreated);}
+        catch(error){
+            next(error)
+        }
+       
             
   
   })
-
 
 
 
